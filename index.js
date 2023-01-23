1b57a849d6e8fb0965fc16bde004d1065c5ab429
Abrar Haque <abm.abrar@gmail.com>
2023-01-24 00:04:33 +0100

    done

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 //ROUTES WITH FILES
-app.post('auth/regiter' upload.single('picture'), register);
+app.post('auth/regiter', upload.single('picture'), register);
 
 //MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
